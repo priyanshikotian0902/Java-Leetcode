c13e91e968198826d721453d80157c0e253cf3ab
priyanshikotian0902 <priyanshi.kotian@mitoae.ac.in>
2024-06-12 12:17:01 +0530

    changes8

--- src/Sort_colors.java
+++ src/Sort_colors.java
@@ -1,30 +1,75 @@
-public class Sort_colors {
+//public class Sort_colors {
+//
+//        public void sortColors(int[] nums) {
+//            int[] arr=new int[3];
+//            int n=nums.length;
+//            int i=0;
+//            for(i=0;i<n;i++)
+//            {
+//                arr[nums[i]]++;
+//            }
+//            int j=0;
+//            i=0;
+//            while(i<n)
+//            {
+//                while(arr[j]!=0)
+//                {
+//                    nums[i]=j;
+//                    i++;
+//                    arr[j]--;
+//
+//                }
+//                j++;
+//            }
+//
+//        }
+//
+//        public static void main(String[] args) {
+//            Sort_colors obj=new Sort_colors();
+//            int[] nums=new int[]{0,1,2,1,2,1,1,0,0,1,2,2,0,0,1,0};
+//            obj.sortColors(nums);
+//            for(int i:nums)
+//            {
+//                System.out.print(i+" ");
+//            }
+//        }
+//}
 
-        public void sortColors(int[] nums) {
-            int[] arr=new int[3];
-            int n=nums.length;
-            int i=0;
-            for(i=0;i<n;i++)
+
+//counting sort takes extra space with below approach doesn't take extra space
+class Sort_colors {
+    public void swap(int[] nums,int i,int j)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void sortColors(int[] nums) {
+        int i=0;
+        int start=0;
+        int end=nums.length-1;
+        while(i<=end)
+        {
+            if(nums[i]==0)
             {
-                arr[nums[i]]++;
+                swap(nums,i,start);
+                i++;
+                start++;
             }
-            int j=0;
-            i=0;
-            while(i<n)
+            else if(nums[i]==2)
             {
-                while(arr[j]!=0)
-                {
-                    nums[i]=j;
-                    i++;
-                    arr[j]--;
-
-                }
-                j++;
+                swap(nums,i,end);
+                end--;
             }
+            else
+            {
+                i++;
 
+            }
         }
+    }
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             Sort_colors obj=new Sort_colors();
             int[] nums=new int[]{0,1,2,1,2,1,1,0,0,1,2,2,0,0,1,0};
             obj.sortColors(nums);
